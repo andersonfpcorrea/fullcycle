217c1c7c104bc42b06ac16972a17af4e6de5f440
Anderson Correa <andersonfpcorrea@gmail.com>
2023-06-17 00:41:17 -0300

    feat: until configmap env

--- kubernetes/server.go
+++ kubernetes/server.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":80", nil)
 }
@@ -15,4 +18,13 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, %s! You are %s years old.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: %s", err)
+	}
+	fmt.Fprintf(w, "My family: %s", string(data))
+
 }
