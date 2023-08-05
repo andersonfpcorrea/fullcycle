29561ba30b6bcf989fe3b212dab9a3aa9a84045d
Anderson Correa <andersonfpcorrea@gmail.com>
2023-08-05 18:02:52 -0300

    feat: base mode project

---
+++ keycloak/authentication-flow/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
